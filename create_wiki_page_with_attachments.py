55da304b1f460fda0c719393ad37eff4b9427727
xifu@pc-dac-xifu <xifu@pc-dac-xifu.irap>
2020-10-21 09:48:14 +0200

    Add the access API Key as input parameter

--- create_wiki_page_with_attachments.py
+++ create_wiki_page_with_attachments.py
@@ -14,10 +14,9 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 """
 Launches a basic GUI to get user inputs.
 """
-# Dialog boxes to get the test parameters
 import easygui
 
-API_key = easygui.enterbox("KEY ? (copy/paste from My Account")
+API_key = easygui.enterbox("KEY ? (copy/paste from My Account on the Redmine site")
 
 testname = easygui.enterbox("Test directory ? (copy/paste from D:\DRE_TESTS\PROTO)", default="20200226_135406__DACBIAS-10_DACFBCK-11_pix-10_fast")
 if testname is None or len(str(testname)) == 0:
@@ -26,7 +25,7 @@ if testname is None or len(str(testname)) == 0:
 
 config = easygui.enterbox("Configuration number ?", default="071")
 operator = easygui.choicebox(msg = "Operator's name", title = "Hello Operator", 
-                             choices = ['Bernard','Christophe','David','Laurent','Odile','Wilfried','Yann'], preselect=4)
+                             choices = ['Bernard','Christophe','David','Laurent','Odile','Wilfried','Yann'], preselect=3)
 
 easygui.msgbox('Test directory = ' + testname + '\n' + 
                'Configuration number = ' + config + '\n' + 
@@ -35,20 +34,19 @@ easygui.msgbox('Test directory = ' + testname + '\n' +
 """
 Redmine access to Redmine servers : please update the "key" parameter with your private access API key.
 """
-# 1a. = Connect to the dummy site bitnami (OCJ)
-# 1b. = Connect to the xifu-redmine site
+# a. = Connect to the dummy site bitnami (OCJ)
+# a. redmine = Redmine('http://127.0.0.1:83/redmine', key = 'KEY', requests={'verify': False})
+# b. = Connect to the xifu-redmine site
+# b. redmine = Redmine('https://xifu-redmine.irap.omp.eu', key = API_key, requests={'verify': False})
+
 from redminelib import Redmine
-# 1a. redmine = Redmine('http://127.0.0.1:82/redmine', key = 'KEY', requests={'verify': False})
-# 1b.
 redmine = Redmine('https://xifu-redmine.irap.omp.eu', key = API_key, requests={'verify': False})
 
-# 1. Get the Prototype_DACs_test_campaign_TEMPLATE wiki page text
-# 1a. wiki_page = redmine.wiki_page.get('Prototype_DACs_test_campaign_TEMPLATE', project_id='test_ocj')    
-# 1b.
+# 1.1. Get the Prototype_DACs_test_campaign_TEMPLATE wiki page text
 wiki_page = redmine.wiki_page.get('Prototype_DACs_test_campaign_TEMPLATE', project_id='DRE')    
 text_template = wiki_page.text
 
-# 2. Replace the wiki text
+# 1.2. Create a header line with the user input values
 header = "|_.Date |_.Test Reference |_.Configuration |_.Operator |_.Test Purposes |_.Results"
 
 strDate = testname[6:8] + str("/") + testname[4:6] + str("/") + testname[0:4]
@@ -57,22 +55,13 @@ text_template = text_template.replace('TEST_NAME', testname)
 text_template = text_template.replace('TEST_CONFIG_ID', config)
 text_template = text_template.replace('OPERATOR', operator)
 
-#print("text_template\n", text_template)
-#print("text_template\n", text_template.replace('\r', ''))
-
-# Get the Text of the DAC current DAC campaign page
-# 1a. wiki_page = redmine.wiki_page.get('Prototype_DACs_test_campaign', project_id='test_ocj')    
-# 1b.
+# 1.3. Get the Text of the current DAC test campaign page and replace the first lines
 wiki_page = redmine.wiki_page.get('Prototype_DACs_test_campaign', project_id='DRE')    
 text_dac_campaign = wiki_page.text
-
-# Replace the wiki text
 text_dac_campaign = text_dac_campaign.replace(header, text_template)
 
-#print("text_dac_campaign AFTER\n", text_dac_campaign)
-
 """
-This part tests howto update the text of a Redmine Wiki page
+This part updates the text of an existing Redmine Wiki page
 """
 redmine.wiki_page.update(
                         'Prototype_DACs_test_campaign',
@@ -80,18 +69,13 @@ redmine.wiki_page.update(
                         title='Prototype_DACs_test_campaign',
                         text=text_dac_campaign
                         )
-print("Page Prototype DACs test campaign updated...")
+print("1. Page Prototype DACs test campaign updated...")
 
-"""
-This part tests howto create a DAC Test report wiki page, with attachment files
-"""
-# 1. Get the Text of the DAC_Test_Report_TEMPLATE Wiki page
-# 1a. wiki_page = redmine.wiki_page.get('DACs_test_report_TEMPLATE', project_id='test_ocj')    
-# 1b.
+# 2.1. Get the Text of the DAC_Test_Report_TEMPLATE Wiki page
 wiki_page = redmine.wiki_page.get('DACs_test_report_TEMPLATE', project_id='DRE')    
 text_template = wiki_page.text
 
-# Replace the wiki text
+# 2.2 Replace the wiki text with the user input values
 table_split = testname.split('_')
 hour = table_split[0] + '_' + table_split[1]
 dacs = table_split[3] + '_' + table_split[4]
@@ -103,15 +87,15 @@ text_template = text_template.replace('TEST_REPORT_TITLE', title)
 text_template = text_template.replace('TEST_NAME', testname)
 text_template = text_template.replace('TEST_CONFIG_ID', config)
 
-# 2. Find all the attachment files on disk
+# 2.3 Find all the attachment files on disk
 import os.path
 normpath = os.path.normpath("D:/DRE_TESTS/PROTO")
 savepath = os.path.join(normpath, testname, "PLOTS")
 
 filelist = os.listdir(savepath)
-uploadlist = []
 
-# Each attachment must be a dict
+# 2.4 Fill a dict with all attachments
+uploadlist = []
 index = 0
 for fi in filelist:
     uploadict = {}
@@ -120,6 +104,9 @@ for fi in filelist:
     uploadict['description'] = "desc"
     uploadlist.append(uploadict)
 
+"""
+This part creates a new Wiki page, with attachment files
+"""
 redmine.wiki_page.create(
                         project_id='DRE',
                         title=testname,
@@ -129,4 +116,5 @@ redmine.wiki_page.create(
                         uploads=uploadlist
                         )
 
-print("Page DACs test report created...")
+print("2. Page DAC test report created...")
+print("Done !")
